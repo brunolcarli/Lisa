ce1330aef72880151a7555aedbe5ff0cd11cbc8a
brunolcarli <brunolcarli@gmail.com>
2019-10-30 19:12:41 -0300

    ADD RF001

--- lisa_processing/schema.py
+++ lisa_processing/schema.py
@@ -1,6 +1,22 @@
 import graphene
+from nltk import sent_tokenize
+
 
 class Query(graphene.ObjectType):
+
+    sentence_segmentation= graphene.List(
+        graphene.String,
+        text=graphene.String(
+            description='Input text for sentece segmentation!',
+            required=True
+        ),
+        description='Process a sentence segmentation over a text input.'
+    )
+    def resolve_sentence_segmentation(self, info, **kwargs):
+        text = kwargs.get('text')
+        segmented_text = sent_tokenize(text)
+
+        return segmented_text
 
     lisa = graphene.List(graphene.String)
     def resolve_lisa(self, info, **kwargs):
