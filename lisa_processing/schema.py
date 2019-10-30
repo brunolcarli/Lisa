ef0b227ee3ebdb152c1eedc7984af4294aa278ec
brunolcarli <brunolcarli@gmail.com>
2019-10-30 19:16:12 -0300

    ADD module docstrings

--- lisa_processing/schema.py
+++ lisa_processing/schema.py
@@ -1,8 +1,20 @@
+"""
+   ___ __ _     _  _ ___    __ __    __    _ 
+|   | (_ |_|   |_||_) |    (_ /  |_||_ |V||_|
+|___|___)| |   | ||  _|_   __)\__| ||__| || |
+
+contact info: brunolcarli@gmail.com
+"""
 import graphene
 from nltk import sent_tokenize
 
 
 class Query(graphene.ObjectType):
+    """
+    Queries da lisa:
+        Dispõe as consultas de processamentode linguagem natural e analise
+        de sentimentos da API.
+        """
 
     sentence_segmentation= graphene.List(
         graphene.String,
@@ -13,6 +25,7 @@ class Query(graphene.ObjectType):
         description='Process a sentence segmentation over a text input.'
     )
     def resolve_sentence_segmentation(self, info, **kwargs):
+        """Processa a requisição de sentence segmentation conforme RF001."""
         text = kwargs.get('text')
         segmented_text = sent_tokenize(text)
 
@@ -20,6 +33,7 @@ class Query(graphene.ObjectType):
 
     lisa = graphene.List(graphene.String)
     def resolve_lisa(self, info, **kwargs):
+        """Isso é um ovo de páscoa."""
         lisa_ascii = [          
             r"      /\  /\ ",
             '  ___/  \/  \___',
