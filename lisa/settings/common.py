acaf1a946898774ec370e800090a96d6ab10c462
brunolcarli <brunolcarli@gmail.com>
2019-11-01 19:33:05 -0300

    ADD carrega o spacy nas settings

--- lisa/settings/common.py
+++ lisa/settings/common.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import spacy
+
+# carrega o spacy para o português
+SPACY = spacy.load('pt')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
