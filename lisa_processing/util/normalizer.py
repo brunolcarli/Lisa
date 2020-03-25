c44a8e75bf704ad37fe60369c57644ab135d0285
Bruno L. Carli <brunolcarli@gmail.com>
2020-03-24 23:00:50 -0300

    CHANGE nromalizer para modulo dedicado

---
+++ lisa_processing/util/normalizer.py
@@ -0,0 +1,33 @@
+from nltk import word_tokenize
+from string import punctuation
+
+
+class Normalizer:
+    """
+    Normaliza padrões de entrada e saída:
+        <str> -> <list>
+        <list> -> <str>
+    """
+    @staticmethod
+    def list_to_string(token_list):
+        """
+        Recebe uma lista de strings e devolve uma string.
+
+        param : token_list: <list>
+        return : <str>
+        """
+        text = ''
+        for token in token_list:
+            if token not in punctuation:
+                text += f'{token} '
+            else:
+                text = text.rstrip() + f'{token} '
+
+        return text.rstrip()
+
+    @staticmethod
+    def string_to_list(text):
+        """
+        recebe uma string e devolve uma lista de tokens
+        """
+        return word_tokenize(text)
