59b386db48e5e28387a3a6db6ffd277371f69320
Bruno L. Carli <brunolcarli@gmail.com>
2020-03-29 01:49:18 -0300

    WIP pipelines

--- lisa_processing/util/pipelines.py
+++ lisa_processing/util/pipelines.py
@@ -11,21 +11,19 @@ def execute_pre_processing(text, algorythms):
     """
     Passa uma entrada de texto por uma lista de algoritmos de pré-processamento.
 
-    param : text : <str>
+    param : text : <str> or <list>
     algorythms : <list>
-    return : <str>
+    return : <list>
     """
-    refresh = Normalizer()
-    skip = lambda text: refresh.string_to_list(text)
+    skip = lambda same: same
     execute = {
-        'sentece_segmentation': sent_tokenize,
-        'tokenize': word_tokenize,
-        'stopwords': remove_stopwords_from_str,
-        'remove_puncts': remove_puncts_from_string
+        'sentece_segmentation': Resolver.resolve_sentence_segmentation,
+        'tokenize': Resolver.resolve_tokenize,
+        'stopwords': Resolver.resolve_remove_stopwords,
+        'remove_puncts': Resolver.resolve_remove_puncts
     }
     for algorythm in algorythms:
         text = execute.get(algorythm, skip)(text)
-        text = refresh.list_to_string(text)
 
     return text
 
@@ -34,15 +32,33 @@ def execute_reducer(text, reducer):
     """
     Aplica os extratores de lemma ou rdical
 
-    param : text : <str>
+    param : text : <str> or <list>
     param : reducer : <str>
-    return : <str>
+    return : <list>
     """
-    refresh = Normalizer()
-    skip = lambda text: refresh.string_to_list(text)
+    skip = lambda same: same
     execute = {
         'stemmer': Resolver.resolve_stemming,
         'lemmer': Resolver.resolve_lemming
     }
-    output = execute.get(reducer, skip)(text)
-    return refresh.list_to_string(output)
+
+    return execute.get(reducer, skip)(text)
+
+
+def execute_processing(text, processor):
+    """
+    Aplica o processamento de análise de sentimentos através do classificador
+    de texto.
+    O retorno pode variar dependendo do algoritmo processador escolhido.
+
+    param : text : <str> or <list>
+    param : processor : <str>
+    """
+    skip = lambda same: same
+    execute = {
+        'lexical_text_classifier': Resolver.resolve_lexical_text_classifier,
+        'dependencies': Resolver.resolve_dependency_parse,
+        'word_offense': Resolver.resolve_word_offense
+    }
+
+    return execute.get(processor, skip)(text)
