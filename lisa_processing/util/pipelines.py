972279fbbc438d6d13312ca91c9d286d271e87d4
Bruno L. Carli <brunolcarli@gmail.com>
2020-04-06 23:36:02 -0300

    CHANGE execução dos pipelines pela classe custom

--- lisa_processing/util/pipelines.py
+++ lisa_processing/util/pipelines.py
@@ -7,58 +7,67 @@ from lisa_processing.resolvers import Resolver
 from lisa_processing.util.normalizer import Normalizer
 
 
-def execute_pre_processing(text, algorythms):
+class CustomPipeline:
     """
-    Passa uma entrada de texto por uma lista de algoritmos de pré-processamento.
-
-    param : text : <str> or <list>
-    algorythms : <list>
-    return : <list>
+    Possui métodos de execução de componentes de um pipeline customizavel.
     """
-    skip = lambda same: same
-    execute = {
-        'sentece_segmentation': Resolver.resolve_sentence_segmentation,
-        'tokenize': Resolver.resolve_tokenize,
-        'stopwords': Resolver.resolve_remove_stopwords,
-        'remove_puncts': Resolver.resolve_remove_puncts
-    }
-    for algorythm in algorythms:
-        text = execute.get(algorythm, skip)(text)
+    @staticmethod
+    def execute_pre_processing(text, algorythms):
+        """
+        Passa uma entrada de texto por uma lista de algoritmos de pré-processamento.
 
-    return text
+        param : text : <str> or <list>
+        algorythms : <list>
+        return : <list>
+        """
+        skip = lambda same: same
+        execute = {
+            'sentece_segmentation': Resolver.resolve_sentence_segmentation,
+            'tokenize': Resolver.resolve_tokenize,
+            'stopwords': Resolver.resolve_remove_stopwords,
+            'remove_puncts': Resolver.resolve_remove_puncts
+        }
+        for algorythm in algorythms:
+            text = execute.get(algorythm, skip)(text)
 
+        return text
 
-def execute_reducer(text, reducer):
-    """
-    Aplica os extratores de lemma ou rdical
+    @staticmethod
+    def execute_reducer(text, reducer):
+        """
+        Aplica os extratores de lemma ou rdical
 
-    param : text : <str> or <list>
-    param : reducer : <str>
-    return : <list>
-    """
-    skip = lambda same: same
-    execute = {
-        'stemmer': Resolver.resolve_stemming,
-        'lemmer': Resolver.resolve_lemming
-    }
+        param : text : <str> or <list>
+        param : reducer : <str>
+        return : <list>
+        """
+        skip = lambda same: same
+        execute = {
+            'stemmer': Resolver.resolve_stemming,
+            'lemmer': Resolver.resolve_lemming
+        }
 
-    return execute.get(reducer, skip)(text)
+        return execute.get(reducer, skip)(text)
 
+    @staticmethod
+    def execute_data_extraction(input_data, feature):
+        """
+        Aplica o processamento de extração de dados e análise sintática à uma
+        entrada.
+        O retorno pode variar dependendo da feature fornecida.
 
-def execute_processing(text, processor):
-    """
-    Aplica o processamento de análise de sentimentos através do classificador
-    de texto.
-    O retorno pode variar dependendo do algoritmo processador escolhido.
+        param : input_data : <str> ou <list>
+        param : feature <str>
+        """
+        skip = lambda same: same
+        execute = {
+            'dependencies': Resolver.resolve_dependency_parse,
+            'entities': Resolver.resolve_named_entity,
+            'pos': Resolver.resolve_part_of_speech,
+            'lexical_text_classifier': Resolver.resolve_lexical_text_classifier,
+            'word_offense': Resolver.resolve_word_offense,
+            'text_offense': Resolver.resolve_text_offense,
+            'word_pol': Resolver.resolve_word_polarity,
+        }
 
-    param : text : <str> or <list>
-    param : processor : <str>
-    """
-    skip = lambda same: same
-    execute = {
-        'lexical_text_classifier': Resolver.resolve_lexical_text_classifier,
-        'dependencies': Resolver.resolve_dependency_parse,
-        'word_offense': Resolver.resolve_word_offense
-    }
-
-    return execute.get(processor, skip)(text)
+        return execute.get(feature, skip)(input_data)
